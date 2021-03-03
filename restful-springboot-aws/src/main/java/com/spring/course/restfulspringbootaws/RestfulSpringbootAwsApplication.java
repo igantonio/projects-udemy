e1034b97801568666bba4d95562db51992a7469d
igantonio <igor.gabriel.saturnino@gmail.com>
2021-03-02 22:59:22 -0300

    Estrutura base do projeto

---
+++ restful-springboot-aws/src/main/java/com/spring/course/restfulspringbootaws/RestfulSpringbootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.course.restfulspringbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulSpringbootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulSpringbootAwsApplication.class, args);
+	}
+
+}
