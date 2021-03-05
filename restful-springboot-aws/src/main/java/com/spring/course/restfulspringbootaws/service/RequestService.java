15a3610cec52c4e19f1dc02c83ad321fca2421b6
igantonio <igor.gabriel.saturnino@gmail.com>
2021-03-04 23:45:01 -0300

    Implementando service de Request

---
+++ restful-springboot-aws/src/main/java/com/spring/course/restfulspringbootaws/service/RequestService.java
@@ -0,0 +1,42 @@
+package com.spring.course.restfulspringbootaws.service;
+
+import com.spring.course.restfulspringbootaws.domain.Request;
+import com.spring.course.restfulspringbootaws.domain.User;
+import com.spring.course.restfulspringbootaws.domain.enums.RequestState;
+import com.spring.course.restfulspringbootaws.repository.RequestRepository;
+import com.spring.course.restfulspringbootaws.service.util.HashUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class RequestService {
+
+    @Autowired
+    private RequestRepository requestRepository;
+
+    public Request save(Request request) {
+        request.setState(RequestState.OPEN);
+        request.setCreationDate(new Date());
+        return requestRepository.save(request);
+    }
+
+    public Request update(Request request) {
+        return requestRepository.save(request);
+    }
+
+    public Request getById(Long id) {
+        return requestRepository.findById(id).get();
+    }
+
+    public List<Request> listAll() {
+        return requestRepository.findAll();
+    }
+
+    public List<Request> listAllByOwnerId(Long id) {
+        return requestRepository.findAllByOwnerId(id);
+    }
+
+}
