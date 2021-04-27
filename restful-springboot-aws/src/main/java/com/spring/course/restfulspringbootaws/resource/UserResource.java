79ca951a8aec8c4fac985b4eb058b1f5978d7ca2
igantonio <igor.gabriel.saturnino@gmail.com>
2021-04-26 22:39:07 -0300

    Expondo authenticationManager como spring e autenticando o usuario com spring metodo

--- restful-springboot-aws/src/main/java/com/spring/course/restfulspringbootaws/resource/UserResource.java
+++ restful-springboot-aws/src/main/java/com/spring/course/restfulspringbootaws/resource/UserResource.java
@@ -13,6 +13,11 @@ import com.spring.course.restfulspringbootaws.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -26,6 +31,9 @@ public class UserResource {
 
     @Autowired
     private RequestService requestService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @PostMapping
     public ResponseEntity<User> save(@RequestBody @Valid UserSaveDto userSaveDto) {
@@ -57,6 +65,12 @@ public class UserResource {
 
     @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody @Valid UserLoginDto userLoginDto) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userLoginDto.getEmail(), userLoginDto.getPassword());
+
+        Authentication auth = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);//Colocando a autenticação no contexto de segurança
+
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(userService.login(userLoginDto.getEmail(), userLoginDto.getPassword()));
--- restful-springboot-aws/src/main/java/com/spring/course/restfulspringbootaws/security/SecurityConfig.java
+++ restful-springboot-aws/src/main/java/com/spring/course/restfulspringbootaws/security/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.spring.course.restfulspringbootaws.security;
 
 import com.spring.course.restfulspringbootaws.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,5 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(customPasswordEncoder);
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
